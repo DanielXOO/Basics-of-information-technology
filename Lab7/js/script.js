7811bcfc2335a9bdf34d7f55b5cf939e90a6e0bd
Dan Kulinkovich <dankulinkovich@gmail.com>
2021-04-05 00:08:41 +0300

    Add files via upload

--- Lab7/js/script.js
+++ Lab7/js/script.js
@@ -1,5 +1,23 @@
 function tsk1(){
-    let win = window.open(`TEST/index.html`, "HELLO", "width=700,height=400");
-    let lnk = win.window.document.getElementById(`Area`);
-    lnk.insertAdjacentHTML("beforeend",`hi`);
+    const win = window.open(`about::blank`, "Windows", "width=700,height=400");
+    let str = `<h2 style = "text-align: center">Имя: окна ${win.name}</h2>`
+    str += `<input type = "button" value = "Закрыть окно" onclick="window.close()">`
+    win.document.write(str);
+}
+
+function tsk2(){
+    let str = `
+    <table>
+    <tr>Браузер: ${navigator.appCodeName} ${navigator.appName} ${navigator.appVersion}</tr>
+    <tr>ОС: ${navigator.platform}</tr>
+    <tr>Включена ли Java: ${navigator.javaEnabled()} </br>
+    Высота и ширина экрана: ${screen.height}x${screen.width} </br>
+    Глубина цвета: ${screen.colorDepth} </br>
+    URL которые были посещены в данном браузере: ${history.length}</br>
+    URL текущего документа: ${document.URL}</br>
+    Путь к загруженному документу: ${location.href}</br>
+    Имя домена и загруженного документа: ${location.hostname} </br>
+    </table>`
+    const lnk = document.getElementById(`right`);
+    lnk.innerHTML = str;
 }
