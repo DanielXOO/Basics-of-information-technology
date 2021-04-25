6aab423b6c7f6507f9753a6df22d7e7ca646509c
Dan Kulinkovich <dankulinkovich@gmail.com>
2021-04-26 00:09:13 +0300

    Add files via upload

--- Lab10/JS/script.js
+++ Lab10/JS/script.js
@@ -26,20 +26,34 @@ function tsk3() {
 
 function tsk4() {
     let str = `<canvas height = "500px" width = "500px"></canvas>`
+    str += `
+    <br>
+    <form name="builder" action="/">
+    <p>Укажите функцию:</p>
+            <div id="lvl">
+                <p><input name="func" type="radio" value="1"> y=x2</p>
+                <p><input name="func" type="radio" value="2"> y=x3</p>
+                <p><input name="func" type="radio" value="3"> y=sin(x)</p>
+                <p><input name="func" type="radio" value="4"> y=cos(x)</p>
+            </div>
+            <p>Цвет
+            <select name = color>
+                <option value="green">Зеленый</option>
+                <option value="red">Красный</option>
+                <option value="black">Черный</option>
+                <option value="blue">Синий</option>
+            </select>
+        </p>
+        </br>
+        <input type="button" value="Build" onclick="buildFunc()">
+    `
     lnk.innerHTML = str;
-    buildField();
-    buildFunc();
 }
 
-function tsk5() {
-    let str = `<img src="img/tux.png" height="100px" width="100px" onmouseover="addImg()" onmouseout="subImg()"  id = "pic">`
-    lnk.innerHTML = str;
-}
 
 function followMouse(aim) {
     let cursPosition = new Coordinate();
-    aim.onmousedown = (event) => {
-
+    aim.onmousedown = event => {
         cursPosition.x = event.offsetX;
         cursPosition.y = event.offsetY;
         document.onmousemove = event => {
@@ -84,3 +98,60 @@ function moveByRand(aim) {
     });
 }
 
+function polyline(color, pts, ctx) {
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    pts.forEach((p, i) => i ? ctx.lineTo(...p) : ctx.moveTo(...p));
+    ctx.stroke();
+}
+
+function buildFunc(){
+    let form = document.forms.builder;
+    let y;
+    switch (form.func.value){
+        case `1`:
+            y = x => Math.pow(x,2);
+        break;
+        case `2`:
+            y = x => Math.pow(x,3);
+        break;
+        case `3`:
+            y = x => Math.sin(x);
+        break;
+        case `4`:
+            y = x => Math.cos(x);
+        break;
+        default:
+            y = 0;
+        break;
+    }
+    
+    let scale = 10;
+    let step = 1;
+    let cnvs = document.querySelector('canvas');
+    let ctx = cnvs.getContext('2d');
+    ctx.clearRect(0, 0, cnvs.width, cnvs.height);
+    ctx.lineWidth = 0.5;
+
+    for (var i = step * scale; i < cnvs.width; i += step * scale) { //вертикальные
+        polyline('gray', [[i, 0], [i, cnvs.height]], ctx);
+    }
+
+    for (var i = step * scale; i < cnvs.height; i += step * scale) { //Горизонтальные
+        polyline('gray', [[0, i], [cnvs.width, i]], ctx);
+    }
+
+    ctx.lineWidth = 2;
+    let pts = [];
+    for (let x = -cnvs.width / 2; x < cnvs.width / 2; x += 5) {
+        pts.push([cnvs.width / 2 + x, cnvs.height / 2 - y(x / scale) * scale]);
+    }
+    polyline(form.color.value, pts, ctx);
+
+    //Ось X 
+    polyline('black', [[0, cnvs.height / 2], [cnvs.width, cnvs.height / 2]], ctx);
+
+    // ось Y
+    polyline('black', [[cnvs.width / 2, 0], [cnvs.width / 2, cnvs.height]], ctx);
+
+}
